9f913f681a339199f5fea5e979d116f3fd2c859b
yurch <tesewar@mail.ru>
2015-11-06 18:12:14 +0300

    доработка моделей

--- main/plans/models.py
+++ main/plans/models.py
@@ -7,8 +7,14 @@ class Plans(models.Model):
     План тестирования
     """
     title = models.CharField("Название плана тестирования", max_length=200)
+    stand_url = models.CharField("Адрес стенда, на котором проверяем", max_length=50)
+    stand_user = models.CharField("Пользователь, под которым проводится проверка", max_length=50)
+
+    species = models.ForeignKey(Species)
+
     date = models.DateTimeField("Дата создания плана")
     text = models.TextField("Текст плана")
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
 
     def __str__(self):
@@ -16,3 +22,25 @@ class Plans(models.Model):
 
     class Meta:
         db_table = "plans"
+
+
+class Species(models.Model):
+    """
+    Вид деятельности
+    """
+    name = models.CharField("Название вида деятельности", max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "species"
+
+
+class Processes(models.Model):
+    """
+    Процессы
+    """
+    name = models.CharField("Название процесса", max_length=200)
+    species = models.ForeignKey(Species)
+    
